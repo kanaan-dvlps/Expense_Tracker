29b47b66230dc4ed0d5e1e78d1eda95142872815
kanaan-dvlps <kanaansci@gmail.com>
2022-11-09 18:30:37 +0100

    fix: fixed expense schema

--- Models/expenses.schema.js
+++ Models/expenses.schema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const expenseSchema = new mongoose.Schema({
   expense: { type: Number },
-  category: { type: 'expense' },
+  parent: { type: 'expense' },
   expenseType: { type: String },
 });
 
