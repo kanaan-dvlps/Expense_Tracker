379bc7c936d8d17fd7d23a9ba67e9a3e1906e0c8
kanaan-dvlps <kanaansci@gmail.com>
2022-11-09 18:25:52 +0100

    feat: added expense and income schema

--- Models/expenses.schema.js
+++ Models/expenses.schema.js
@@ -2,5 +2,9 @@ const mongoose = require('mongoose');
 
 const expenseSchema = new mongoose.Schema({
   expense: { type: Number },
-  category: { type: String },
-})
+  category: { type: 'expense' },
+  expenseType: { type: String },
+});
+
+const expenseModel = mongoose.model('expense', expenseSchema);
+module.exports = expenseModel;
