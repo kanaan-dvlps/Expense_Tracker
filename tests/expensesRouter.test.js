c2523ff793a94b1eef9f509fc0c503a24c81c186
kanaan-dvlps <kanaansci@gmail.com>
2022-11-12 19:03:53 +0100

    feat: added test for expense router

--- tests/expensesRouter.test.js
+++ tests/expensesRouter.test.js
@@ -1,21 +1,12 @@
 const expenseRouter = require('../Routes/expenses');
 const axios = require('axios');
 
-const POST_EXPENSE_API_RESULT = async () => {
+const POST_EXPENSE_API_RESULT = async (data) => {
   try {
-    // const result = await axios.post('http://localhost:1337/api/v1/balance/expenses', {
-    //   data: {
-    //     amount: 200,
-    //     type: 'Grocery'
-    //   }
-    // });
     const result = await axios({
       method: 'post',
       url: 'http://localhost:1337/api/v1/balance/expenses',
-      data: {
-        amount: 200,
-        type: 'Grocery'
-      }
+      data: data
     });
     const response = await result.data;
     return response;
@@ -24,11 +15,13 @@ const POST_EXPENSE_API_RESULT = async () => {
   }
 };
 
-
 describe("Testing expense router", () => {
   it("Should return 201 when creating a new expense", async () => {
 
-    const result = await POST_EXPENSE_API_RESULT();
+    const result = await POST_EXPENSE_API_RESULT({
+      amount: 200,
+      type: 'Grocery'
+    });
     expect(result).toEqual({ amount: 200, type: 'Grocery', successful: true });
-  })
-})
+  });
+});
